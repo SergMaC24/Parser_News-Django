663a8ad0f72a322864b5feef0e21b574b11763b5
“Serg <serg808507@gmail.com>
2022-07-14 12:14:33 +0300

    remove space

--- news/news_app/views.py
+++ news/news_app/views.py
@@ -23,9 +23,27 @@ def get_hn():
 
 get_hn()
 
+silicon_list = []
+
+
+def get_silicon():
+    r = requests.get(silicon).text
+    soup = BeautifulSoup(r, 'lxml')
+    posts = soup.find_all('div', class_='article-info')
+    for post in posts:
+        title = post.find('a', class_='article-title').text
+        url = post.find('a', class_='article-title').get('href')
+        data = {'title': title,
+                'url': url}
+        silicon_list.append(data)
+
+
+get_silicon()
+
 
 def home(requests):
     context = {
         'hn_list': hn_list,
+        'silicon_list': silicon_list,
     }
     return render(requests, 'news_app/home.html', context)
